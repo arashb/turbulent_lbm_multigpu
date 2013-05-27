048f6387ffac0c91811f111db99cd5d602a7b716
Arash Bakhtiari <bakhtiar@in.tum.de>
2013-05-27 14:56:23 +0200

    Added the command line arguments to specify the number of subdomains in each direction

--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,9 @@
 #include "CController.hpp"
 #include "common.h"
 #include "CManager.hpp"
-//#include "CConfiguration.hpp"
+#include "CConfiguration.hpp"
 //#include "Singleton.hpp"
+#include "tinyxml2.h"
 
 CVector<3,int> E0(1,0,0) 	;
 CVector<3,int> E1(-1,0,0)	;
@@ -101,6 +102,7 @@ int main(int argc, char** argv)
 	bool debug = false;
 
 	CVector<3,int> domain_size(32,32,32);
+	CVector<3,int> subdomain_nums(1,1,1);
 	CVector<3,T> gravitation(0,-9.81,0);
 	T viscosity = 0.001308;
 	T timestep = -1.0;
@@ -115,95 +117,112 @@ int main(int argc, char** argv)
 	std::string number_of_registers_string;	///< string storing the number of registers for opencl threads separated with comma
 	std::string number_of_threads_string;		///< string storing the number of threads for opencl separated with comma
 	std::string test_suite;
+	bool use_config_file;
+	std::string conf_file;
 
 	int device_nr = 0;
 
 	char optchar;
-	while ((optchar = getopt(argc, argv, "x:y:z:d:vr:k:gG:pt:sl:R:T:X:u:")) > 0)
+	while ((optchar = getopt(argc, argv, "x:y:z:d:vr:k:gG:pt:sl:R:T:X:Y:Z:S:u:c:")) > 0)
 	{
 		switch(optchar)
 		{
-			case 'd':
-				device_nr = atoi(optarg);
-				break;
+		case 'd':
+			device_nr = atoi(optarg);
+			break;
 
-			case 'v':
-				debug = true;
-				break;
+		case 'v':
+			debug = true;
+			break;
 
-			case 'R':
-				number_of_registers_string = optarg;
-				break;
+		case 'R':
+			number_of_registers_string = optarg;
+			break;
 
-			case 'T':
-				number_of_threads_string = optarg;
-				break;
+		case 'T':
+			number_of_threads_string = optarg;
+			break;
 
-			case 'p':
-				pause = true;
-				break;
+		case 'p':
+			pause = true;
+			break;
 
-			case 'x':
-				domain_size[0] = atoi(optarg);
-				break;
+		case 'x':
+			domain_size[0] = atoi(optarg);
+			break;
 
-			case 'X':
-				domain_size[0] = atoi(optarg);
-				domain_size[1] = domain_size[0];
-				domain_size[2] = domain_size[0];
-				break;
+		case 'X':
+			subdomain_nums[0] = atoi(optarg);
+			break;
+		case 'Y':
+			subdomain_nums[1] = atoi(optarg);
+			break;
+		case 'Z':
+			subdomain_nums[2] = atoi(optarg);
+			break;
+		case 'S':
+			domain_size[0] = atoi(optarg);
+			domain_size[1] = atoi(optarg);
+			domain_size[2] = atoi(optarg);
+			break;
+		case 'l':
+			steps = atoi(optarg);
+			break;
 
-			case 'l':
-				steps = atoi(optarg);
-				break;
+		case 'y':
+			domain_size[1] = atoi(optarg);
+			break;
 
-			case 'y':
-				domain_size[1] = atoi(optarg);
-				break;
+		case 'k':
+			computation_kernel_count = atoi(optarg);
+			break;
 
-			case 'k':
-				computation_kernel_count = atoi(optarg);
-				break;
+		case 'z':
+			domain_size[2] = atoi(optarg);
+			break;
 
-			case 'z':
-				domain_size[2] = atoi(optarg);
-				break;
+		case 'r':
+			viscosity = atof(optarg);
+			break;
 
-			case 'r':
-				viscosity = atof(optarg);
-				break;
+		case 'g':
+			gui = true;
+			break;
 
-			case 'g':
-				gui = true;
-				break;
+		case 'G':
+			gravitation[1] = atof(optarg);
+			break;
 
-			case 'G':
-				gravitation[1] = atof(optarg);
-				break;
+		case 't':
+			timestep = atof(optarg);
+			break;
 
-			case 't':
-				timestep = atof(optarg);
-				break;
+		case 's':
+			take_frame_screenshots = true;
+			break;
 
-			case 's':
-				take_frame_screenshots = true;
-				break;
-
-			case 'u':
-				unit_test = true;
-				test_suite = optarg;
-				break;
-			default:
-				goto parameter_error;
+		case 'u':
+			unit_test = true;
+			test_suite = optarg;
+			break;
+		case 'c':
+			use_config_file = true;
+			conf_file = optarg;
+			break;
+		default:
+			goto parameter_error;
 		}
 	}
 
 	goto parameter_error_ok;
-parameter_error:
+	parameter_error:
 	std::cout << "usage: " << argv[0] << std::endl;
 	std::cout << "		[-x resolution_x, default: 32]" << std::endl;
 	std::cout << "		[-y resolution_y, default: 32]" << std::endl;
 	std::cout << "		[-z resolution_z, default: 32]" << std::endl;
+	std::cout << "		[-X subdomain resolution_x, default: 1]" << std::endl;
+	std::cout << "		[-Y subdomain resolution_y, default: 1]" << std::endl;
+	std::cout << "		[-Z subdomain resolution_z, default: 1]" << std::endl;
 	std::cout << std::endl;
 	std::cout << "		[-G gravitation in down direction, default: -9.81]" << std::endl;
 	std::cout << std::endl;
@@ -220,81 +239,90 @@ parameter_error:
 	std::cout << "		[-t timestep]	(default: -1 for automatic detection)" << std::endl;
 	std::cout << "		[-s]	(take a screenshot every frame - default: disabled)" << std::endl;
 	std::cout << "    	[-u] run unit tests" << std::endl;
+	std::cout << "    	[-c] read the configuration file" << std::endl;
 	return -1;
 
-parameter_error_ok:
+	parameter_error_ok:
 
+//	if( use_config_file) {
+//		CConfiguration<T> config;
+//		config.load_file(conf_file);
+//	}
+	if (unit_test) {
 
-	  if (unit_test) {
+		if ( strcmp(  "all", test_suite.c_str() ) == 0 ) {
+			if( debug)
+				std::cout << "running all test." << std::endl;
+			return UnitTest::RunAllTests();
+		}
 
-		  if ( strcmp(  "all", test_suite.c_str() ) == 0 ) {
-			  if( debug)
-				  std::cout << "running all test." << std::endl;
-			  return UnitTest::RunAllTests();
-		  }
+		else {
+			const UnitTest::TestList& allTests( UnitTest::Test::GetTestList() );
+			UnitTest::TestList selectedTests;
+			UnitTest::Test* p = allTests.GetHead();
+			while( p )
+			{
+				//			  for( int i = 1 ; i < argc ; ++i )
+				if( strcmp(  p->m_details.suiteName , test_suite.c_str() ) == 0 ) {
+					selectedTests.Add( p );
+					if( debug)
+						std::cout << "Added test " << p->m_details.testName << "from suite " <<  p->m_details.suiteName << " to tes list." << std::endl;
+				}
+				p = p->next;
+			}
 
-		  else {
-			  const UnitTest::TestList& allTests( UnitTest::Test::GetTestList() );
-			  UnitTest::TestList selectedTests;
-			  UnitTest::Test* p = allTests.GetHead();
-			  while( p )
-			  {
-				  //			  for( int i = 1 ; i < argc ; ++i )
-				  if( strcmp(  p->m_details.suiteName , test_suite.c_str() ) == 0 ) {
-					  selectedTests.Add( p );
-					  if( debug)
-						  std::cout << "Added test " << p->m_details.testName << "from suite " <<  p->m_details.suiteName << " to tes list." << std::endl;
-				  }
-				  p = p->next;
-			  }
+			//run selected test(s) only
+			UnitTest::TestReporterStdout reporter;
+			UnitTest::TestRunner runner( reporter );
+			return runner.RunTestsIf( selectedTests, 0, UnitTest::True(), 0 );
+		}
 
-			  //run selected test(s) only
-			  UnitTest::TestReporterStdout reporter;
-			  UnitTest::TestRunner runner( reporter );
-			  return runner.RunTestsIf( selectedTests, 0, UnitTest::True(), 0 );
-		  }
+	} else {
+		CVector<3,T> length(0.10,0.05,0.05);
+		CVector<3,int> origin(0,0,0);
+		CDomain<T> domain(-1, domain_size, origin, length);
 
-	  } else {
-		  CVector<3,T> length(0.10,0.05,0.05);
-		  CVector<3,int> origin(0,0,0);
-		  CDomain<T> domain(-1, domain_size, origin, length);
+		// TODO: get the number of subdomain for each dimension from user.
+		//CVector<3,int> subdomain_nums(2,1,1);
+		CManager<T> manager(domain, subdomain_nums);
 
-		  // TODO: get the number of subdomain for each dimension from user.
-		  CVector<3,int> subdomain_nums(2,1,1);
-		  CManager<T> manager(domain, subdomain_nums);
+		std::list<int> lbm_opencl_number_of_registers_list;
+		std::list<int> lbm_opencl_number_of_threads_list;
 
-		  std::list<int> lbm_opencl_number_of_registers_list;
-		  std::list<int> lbm_opencl_number_of_threads_list;
+		if (!number_of_threads_string.empty())
+			extract_comma_separated_integers(lbm_opencl_number_of_threads_list, number_of_threads_string);
 
-		  if (!number_of_threads_string.empty())
-			  extract_comma_separated_integers(lbm_opencl_number_of_threads_list, number_of_threads_string);
+		if (!number_of_registers_string.empty())
+			extract_comma_separated_integers(lbm_opencl_number_of_registers_list, number_of_registers_string);
 
-		  if (!number_of_registers_string.empty())
-			  extract_comma_separated_integers(lbm_opencl_number_of_registers_list, number_of_registers_string);
+		int my_rank, num_procs;
+		MPI_Init(&argc, &argv);    /// Start MPI
+		MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);    /// Get current process id
+		MPI_Comm_size(MPI_COMM_WORLD, &num_procs);    /// Get number of processes
 
-		  int my_rank, num_procs;
-		  MPI_Init(&argc, &argv);    /// Start MPI
-		  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);    /// Get current process id
-		  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);    /// Get number of processes
+		if ( num_procs != subdomain_nums.elements()) {
+			std::cout << "Number of allocated processors should be equal to the number of subdomains!" << std::endl;
+			std::cout << "Current number of subdomains is : " << subdomain_nums.elements() << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		manager.initSimulation(my_rank);
+		manager.startSimulation(	debug,
+				gravitation,
+				viscosity,
+				computation_kernel_count,
+				device_nr,
+				gui,
+				pause,
+				timestep,
+				take_frame_screenshots,
+				steps,
 
-		  manager.initSimulation(my_rank);
-		  manager.startSimulation(	debug,
-				  				  gravitation,
-				  				  viscosity,
-				  				  computation_kernel_count,
-				  				  device_nr,
-				  				  gui,
-				  				  pause,
-				  				  timestep,
-				  				  take_frame_screenshots,
-				  				  steps,
+				lbm_opencl_number_of_threads_list,
+				lbm_opencl_number_of_registers_list
+		);
 
-				  				  lbm_opencl_number_of_threads_list,
-				  				  lbm_opencl_number_of_registers_list
-				  );
-
-		  MPI_Finalize();    /// Cleanup MPI
-	  }
+		MPI_Finalize();    /// Cleanup MPI
+	}
 
 
 }
