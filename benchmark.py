27572ca5afb55dad2ca275753df0f98fa6504c20
Arash Bakhtiari <bakhtiar@in.tum.de>
2013-07-01 01:22:53 +0200

    Added 1d week scaling to benchmark script

--- benchmark.py
+++ benchmark.py
@@ -37,7 +37,7 @@ def execute(command):
     print "executing command: ", command
     os.system(command)
 
-def weak_scaling_benchmark_2d(max_num, num_exp, loops = 100, grid_size_increase_step = 32, base_domain_length = 0.1 ):
+def weak_scaling_benchmark_2d(max_num, num_exp, loops = 100, grid_size_increase_step = 1024, base_domain_length = 0.1 ):
     DOMAIN_LENGTH = base_domain_length
     for num_increase in range(1,max_num+1):
         num_proc = num_increase*num_increase
@@ -50,6 +50,30 @@ def weak_scaling_benchmark_2d(max_num, num_exp, loops = 100, grid_size_increase_
         command_str += " -y " + str(y_size) +  " -Y " +  str(num_increase) 
         command_str += " -l " + str(loops)
         command_str +=  " -n " + str(DOMAIN_LENGTH*num_increase) + " -m " + str(DOMAIN_LENGTH*num_increase) + " -p " + str(DOMAIN_LENGTH)
+        ini_filename = INI_FILE_DIR + "benchmark_" + str(num_proc)+ ".ini"
+        # performing the experiments
+        for exp_counter in range(1,num_exp+1):
+            f = open(ini_filename, 'a')
+            f.write("["+SECTION_BASE+str(exp_counter)+"]\n")
+            f.write("NP : " + str(num_proc))
+            f.write("\n")
+            f.close()
+            execute(command_str)
+
+# TODO
+def weak_scaling_benchmark_1d(max_num, num_exp, loops = 100, grid_size_increase_step = 1024, base_domain_length = 0.1 ):
+    DOMAIN_LENGTH = base_domain_length
+    for num_increase in range(1,max_num+1):
+        num_proc = num_increase
+        # computing the grid size for current benchmark
+        x_size = grid_size_increase_step*num_increase
+        y_size = 1024
+        # generating the string of execution command  
+        command_str = MPI_COMMAND + " -n " + str(num_proc)+ " " + LBM_COMMAND 
+        command_str += " -x " + str(x_size) +  " -X " +  str(num_increase) 
+        command_str += " -y " + str(y_size) #+  " -Y " +  str(num_increase) 
+        command_str += " -l " + str(loops)
+        command_str +=  " -n " + str(DOMAIN_LENGTH*num_increase) + " -m " + str(DOMAIN_LENGTH) + " -p " + str(DOMAIN_LENGTH)
         ini_filename = INI_FILE_DIR + "benchmark_" + str(num_proc)+ ".ini"
         # performing the experiments
         for exp_counter in range(1,num_exp+1):
@@ -180,7 +204,7 @@ if __name__ == "__main__":
         BENCHMARK_STRATEGY = str(sys.argv[5])
         print "benchmark strategy: ", BENCHMARK_STRATEGY
         if BENCHMARK_STRATEGY == "weak":
-            benchmark(weak_scaling_benchmark_2d, max_num, num_exp)
+            benchmark(weak_scaling_benchmark_1d, max_num, num_exp)
         elif BENCHMARK_STRATEGY == "strong":
             benchmark(strong_scaling_benchmark_2d, max_num, num_exp)
         else:
